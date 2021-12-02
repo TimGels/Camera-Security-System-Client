97827bb6f71930cec7b8007b8e606a532cfd59a6
Jochem <jochembrans@gmail.com>
2021-12-02 16:53:40 +0100

    Added thumbnails and changed camera

--- app/src/main/java/com/example/camerasecuritysystem/DashcamFragment.kt
+++ app/src/main/java/com/example/camerasecuritysystem/DashcamFragment.kt
@@ -1,5 +1,6 @@
 package com.example.camerasecuritysystem
 
+import android.Manifest
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,57 +9,207 @@ import android.view.View
 import android.view.ViewGroup
 
 import androidx.camera.camera2.Camera2Config
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.CameraXConfig
-import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import com.google.common.util.concurrent.ListenableFuture
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.util.Log
 
-class DashcamFragment : Fragment(), CameraXConfig.Provider {
+import androidx.annotation.NonNull
+import androidx.annotation.Nullable
 
-    private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
-    private lateinit var listener: Context
+import androidx.core.app.ActivityCompat
+import java.util.concurrent.ExecutionException
 
-    interface DashcamListener{
-        fun onInputSent(input: CharSequence)
+import com.example.camerasecuritysystem.databinding.FragmentDashcamBinding
+import android.media.MediaRecorder
+import android.os.Build
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.camera.core.*
+import java.io.File
+import java.io.IOException
+
+
+class DashcamFragment(context: Context?) : Fragment(), CameraXConfig.Provider {
+
+    val TAG: String = DashcamFragment::class.java.getSimpleName()
+    private val REQUEST_PERMISSION_CAMERA = 777
+
+//    fun newInstance(): Fragment? {
+//        return DashcamFragment()
+//    }
+
+    private var binding: FragmentDashcamBinding? = null
+    private var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>? = null
+
+    private var fileName: String = ""
+
+    private var recorder: MediaRecorder? = null
+    private var player: MediaPlayer? = null
+
+    private var videoCapture: VideoCapture? = null
+
+
+    private val LOG_TAG = "AudioRecordTest"
+    private val REQUEST_RECORD_AUDIO_PERMISSION = 200
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.v(TAG, "create view")
+        binding = FragmentDashcamBinding.inflate(inflater, container, false)
+        // binding.setViewModel(this.mViewModel);
+        return binding!!.getRoot()
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        cameraProviderFuture = ProcessCameraProvider.getInstance(listener)
-        cameraProviderFuture.addListener(Runnable {
-            val cameraProvider = cameraProviderFuture.get()
-            bindPreview(cameraProvider)
-        }, ContextCompat.getMainExecutor(listener))
+    override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.v(TAG, "view created")
+
+        //Start recording
+//        startRecording()
     }
 
-    fun bindPreview(cameraProvider : ProcessCameraProvider) {
-        var preview : Preview = Preview.Builder()
-            .build()
-
-        var cameraSelector : CameraSelector = CameraSelector.Builder()
-            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-            .build()
-
-        preview.setSurfaceProvider(R.id.viewFinder.getSurfaceProvider())
-        var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+    private fun onRecord(start: Boolean) = if (start) {
+        startRecording()
+    } else {
+        stopRecording()
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashcam, container, false)
-    }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if(context is DashcamListener){
-            listener = context
+
+    private fun startRecording() {
+
+        Log.e(LOG_TAG, "IK KOM HIER")
+
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.DEFAULT)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setMaxDuration(5000)
+
+            var root = context?.filesDir
+
+
+            var file = File("$root/test" + (System.currentTimeMillis() / 1000L).toString() + ".mp4")
+            setOutputFile(file)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+            try {
+                prepare()
+                start()
+                Log.e(LOG_TAG, "APP GESTART")
+                Toast.makeText(context, "Opname gestart", Toast.LENGTH_LONG)
+            } catch (e: IOException) {
+
+                Log.e(LOG_TAG, "prepare() failed")
+                Log.e(LOG_TAG, e.toString())
+            }
+
         }
     }
 
+    private fun stopRecording() {
+
+
+        recorder?.apply {
+            stop()
+            release()
+        }
+        recorder = null
+    }
+
+    override fun onStop() {
+        super.onStop()
+        recorder?.release()
+        recorder = null
+        player?.release()
+        player = null
+
+        Toast.makeText(context, "Opname gestopt", Toast.LENGTH_SHORT)
+
+        startRecording()
+    }
+
+    private fun startPreview() {
+        Log.v(TAG, "startPreview")
+        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture!!.addListener({
+            Log.v(TAG, "cameraProviderFuture.Listener")
+            try {
+                val cameraProvider = cameraProviderFuture!!.get()
+                val preview = Preview.Builder()
+                    .build()
+
+                preview.setSurfaceProvider(binding?.viewFinder?.surfaceProvider)
+
+                videoCapture = VideoCapture.Builder().build()
+
+                val cameraSelector = CameraSelector.Builder()
+                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                    .build()
+
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(
+                    this.viewLifecycleOwner,
+                    cameraSelector,
+                    preview, videoCapture
+                )
+            } catch (e: ExecutionException) {
+                // No errors need to be handled for this Future.
+                // This should never be reached.
+                Log.e(TAG, "cameraProviderFuture.Listener", e)
+            } catch (e: InterruptedException) {
+                Log.e(TAG, "cameraProviderFuture.Listener", e)
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.RECORD_AUDIO
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            if (cameraProviderFuture == null) {
+                startPreview()
+            }
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), REQUEST_PERMISSION_CAMERA)
+            requestPermissions(
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                REQUEST_RECORD_AUDIO_PERMISSION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_PERMISSION_CAMERA) {
+            for (p in permissions.indices) {
+                if (Manifest.permission.CAMERA == permissions[p]) {
+                    if (grantResults[p] == PackageManager.PERMISSION_GRANTED) {
+                        startPreview()
+                    } else {
+                    }
+                }
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
 
     override fun getCameraXConfig(): CameraXConfig {
         return Camera2Config.defaultConfig()
