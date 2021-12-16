e434523899e96876b01be7dd3edb5f6d98d57b52
Jochem <jochembrans@gmail.com>
2021-12-16 11:49:40 +0100

    Play video on click

---
+++ app/src/main/kotlin/com/camerasecuritysystem/client/VideoPlayerActivity.kt
@@ -0,0 +1,34 @@
+package com.camerasecuritysystem.client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.MediaController
+import android.widget.TextView
+import android.widget.VideoView
+
+class VideoPlayerActivity : AppCompatActivity() {
+
+    private lateinit var videoView : VideoView
+    private lateinit var videoPath : String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_player)
+
+        videoView = findViewById(R.id.videoView)
+
+        var mediaController = MediaController(this, false)
+
+        videoView.setVideoPath(getVideoPath())
+
+        mediaController.setAnchorView(videoView)
+        videoView.setMediaController(mediaController)
+        videoView.start()
+
+    }
+
+    fun getVideoPath() : String? {
+        return intent.getStringExtra("videoPath")
+    }
+}
