f70bdbe2343a36aaff0a176ed40864eaf3662e85
Johannes Kauffmann <19662702+JohannesKauffmann@users.noreply.github.com>
2021-12-20 14:26:25 +0100

    feat: Add Footage data class
    The demo FootageHandler has been adapted to work with this.

--- app/src/main/kotlin/com/camerasecuritysystem/client/models/Message.kt
+++ app/src/main/kotlin/com/camerasecuritysystem/client/models/Message.kt
@@ -4,8 +4,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Message(
-    val type: MessageType,          // Mandatory
-    val id: Int                     = 0,
-    val password: String?           = null,
-    val footage: ArrayList<String>? = null
+    val type: MessageType, // Mandatory
+    val id: Int = 0,
+    val password: String? = null,
+    val footage: ArrayList<Footage>? = null
 )
