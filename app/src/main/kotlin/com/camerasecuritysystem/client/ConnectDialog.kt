5e0b6fd81e9fddb1fe61af27b30d58d65b6e8bd9
Jochem <jochembrans@gmail.com>
2021-12-23 13:10:13 +0100

    New regex

--- app/src/main/kotlin/com/camerasecuritysystem/client/ConnectDialog.kt
+++ app/src/main/kotlin/com/camerasecuritysystem/client/ConnectDialog.kt
@@ -10,8 +10,7 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.core.widget.doOnTextChanged
 import com.camerasecuritysystem.client.databinding.ConnectDialogLayoutBinding
-import java.lang.Exception
-import java.lang.NumberFormatException
+import java.util.regex.Pattern
 
 class ConnectDialog : AppCompatDialogFragment() {
 
@@ -23,17 +22,17 @@ class ConnectDialog : AppCompatDialogFragment() {
 
     private lateinit var binding: ConnectDialogLayoutBinding
 
-    private var cameraValid: Boolean    = false
-    private var portValid:   Boolean    = false
-    private var ipAddrValid: Boolean    = false
-    private var passWdValid: Boolean    = false
+    private var cameraValid: Boolean = false
+    private var portValid: Boolean = false
+    private var ipAddrValid: Boolean = false
+    private var passWdValid: Boolean = false
 
     private var okButton: Button? = null
 
-    private lateinit var cameraidStr:  String
-    private lateinit var portStr:      String
+    private lateinit var cameraidStr: String
+    private lateinit var portStr: String
     private lateinit var ipaddressStr: String
-    private lateinit var passwordStr:  String
+    private lateinit var passwordStr: String
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val inflater = requireActivity().layoutInflater
@@ -41,23 +40,24 @@ class ConnectDialog : AppCompatDialogFragment() {
 
         keyStore = KeyStoreHelper(requireContext().getString(R.string.keyStoreAliasServer))
 
-        this.cameraidStr    = resources.getString(R.string.camera_id_title)
-        this.portStr        = resources.getString(R.string.port_title)
-        this.ipaddressStr   = resources.getString(R.string.ip_address_title)
-        this.passwordStr    = resources.getString(R.string.password_title)
+        this.cameraidStr = resources.getString(R.string.camera_id_title)
+        this.portStr = resources.getString(R.string.port_title)
+        this.ipaddressStr = resources.getString(R.string.ip_address_title)
+        this.passwordStr = resources.getString(R.string.password_title)
 
-        //Get the id of the input fields
-        val editTextCameraId  = binding.editCameraId
-        val editTextPort      = binding.editPort
+        // Get the id of the input fields
+        val editTextCameraId = binding.editCameraId
+        val editTextPort = binding.editPort
         val editTextIPAddress = binding.editIpAddress
-        val editTextPassword  = binding.editPassword
+        val editTextPassword = binding.editPassword
 
-        //Get access to all shared preferences
+        // Get access to all shared preferences
         sharedPreferences = requireContext().getSharedPreferences(
             "com.camerasecuritysystem.client",
-            Context.MODE_PRIVATE)
+            Context.MODE_PRIVATE
+        )
 
-        //Assign the preferences to variables
+        // Assign the preferences to variables
         val cameraId =
             sharedPreferences.getString(resources.getString(R.string.camera_id), null)
         val port =
@@ -69,26 +69,25 @@ class ConnectDialog : AppCompatDialogFragment() {
         val ivByte =
             sharedPreferences.getString(resources.getString(R.string.pwdIVByte), null)
 
-        //Check if the ivByte and encrypted password exist
+        // Check if the ivByte and encrypted password exist
         if (ivByte != null && encPwd != null) {
 
-            //Decrypt the IV byte and the password
+            // Decrypt the IV byte and the password
             try {
                 val pwdText = keyStore.decryptData(
                     ivByte.toByteArray(Charsets.ISO_8859_1),
                     encPwd.toByteArray(Charsets.ISO_8859_1)
                 )
 
-                //Set the password text in the input field
+                // Set the password text in the input field
                 binding.editPassword.setText(pwdText)
                 passWdValid = true
-
             } catch (e: Exception) {
                 Log.e("EXCEPTION", "error: ", e)
             }
         }
 
-        //Create the dialog window
+        // Create the dialog window
         val dialog = AlertDialog.Builder(requireActivity())
             .setView(binding.root)
             .setTitle("Login")
@@ -102,7 +101,7 @@ class ConnectDialog : AppCompatDialogFragment() {
                 val ipAddressText = editTextIPAddress.text.toString()
                 val passwordText = editTextPassword.text.toString()
 
-                //Pass the new input values to store them in the shared preferences
+                // Pass the new input values to store them in the shared preferences
                 listener!!.applyTexts(cameraIdText, portText, ipAddressText, passwordText)
             }
             .create()
@@ -110,50 +109,53 @@ class ConnectDialog : AppCompatDialogFragment() {
         dialog.setOnShowListener {
             this.okButton = (it as AlertDialog).getButton(AlertDialog.BUTTON_POSITIVE)
 
-            //Set the initial values for the input fields and update the OK button
+            // Set the initial values for the input fields and update the OK button
             if (cameraId != null) {
                 binding.editCameraId.setText(cameraId)
                 cameraValid = true
             } else {
-                binding.inputLayoutCameraID.error = String.format(resources.getString(R.string.err_not_empty), cameraidStr)
+                binding.inputLayoutCameraID.error =
+                    String.format(resources.getString(R.string.err_not_empty), cameraidStr)
                 setOkButton(okButton)
             }
-            //Set the port text in the input field
+            // Set the port text in the input field
             if (port != null) {
                 binding.editPort.setText(port)
                 portValid = true
             } else {
-                binding.inputLayoutPort.error = String.format(resources.getString(R.string.err_not_empty), portStr)
+                binding.inputLayoutPort.error =
+                    String.format(resources.getString(R.string.err_not_empty), portStr)
                 setOkButton(okButton)
             }
-            //Set the IP text in the input field
+            // Set the IP text in the input field
             if (ipAddress != null) {
                 binding.editIpAddress.setText(ipAddress)
                 ipAddrValid = true
             } else {
-                binding.inputLayoutIpAddress.error = String.format(resources.getString(R.string.err_not_empty), ipaddressStr)
+                binding.inputLayoutIpAddress.error =
+                    String.format(resources.getString(R.string.err_not_empty), ipaddressStr)
                 setOkButton(okButton)
             }
-            //Check if the ivByte and encrypted password exist
+            // Check if the ivByte and encrypted password exist
             if (ivByte != null && encPwd != null) {
 
-                //Decrypt the IV byte and the password
+                // Decrypt the IV byte and the password
                 try {
                     val pwdText = keyStore.decryptData(
                         ivByte.toByteArray(Charsets.ISO_8859_1),
                         encPwd.toByteArray(Charsets.ISO_8859_1)
                     )
 
-                    //Set the password text in the input field
+                    // Set the password text in the input field
                     editTextPassword.setText(pwdText)
                     passWdValid = true
-
                 } catch (e: Exception) {
                     Log.e("EXCEPTION", "error: ", e)
                     setOkButton(okButton)
                 }
             } else {
-                binding.inputLayoutPassword.error = String.format(resources.getString(R.string.err_not_empty), passwordStr)
+                binding.inputLayoutPassword.error =
+                    String.format(resources.getString(R.string.err_not_empty), passwordStr)
                 setOkButton(okButton)
             }
 
@@ -181,9 +183,8 @@ class ConnectDialog : AppCompatDialogFragment() {
     }
 
     private fun setOkButton(okButton: Button?) {
-        okButton?.isEnabled = ( cameraValid && portValid && ipAddrValid && passWdValid )
+        okButton?.isEnabled = (cameraValid && portValid && ipAddrValid && passWdValid)
     }
-
 
     /* The following validation functions will try to sanitize the user input at least
      * somewhat, and show an appropriate error. */
@@ -192,18 +193,21 @@ class ConnectDialog : AppCompatDialogFragment() {
         val camera_til = binding.inputLayoutCameraID
 
         if (cameraIdText == null || cameraIdText.isEmpty()) {
-            camera_til.error = String.format(resources.getString(R.string.err_not_empty), cameraidStr)
+            camera_til.error =
+                String.format(resources.getString(R.string.err_not_empty), cameraidStr)
             return false
         }
 
         try {
             val cameraIdAsInt = cameraIdText.toInt()
             if (cameraIdAsInt < 1) {
-                camera_til.error = String.format(resources.getString(R.string.err_at_least), cameraidStr, 1)
+                camera_til.error =
+                    String.format(resources.getString(R.string.err_at_least), cameraidStr, 1)
                 return false
             }
         } catch (ex: NumberFormatException) {
-            camera_til.error = String.format(resources.getString(R.string.err_must_be_number), cameraidStr)
+            camera_til.error =
+                String.format(resources.getString(R.string.err_must_be_number), cameraidStr)
             return false
         }
 
@@ -222,15 +226,18 @@ class ConnectDialog : AppCompatDialogFragment() {
         try {
             val portAsInt = portText.toInt()
             if (portAsInt < 1) {
-                port_til.error = String.format(resources.getString(R.string.err_at_least), portStr, 1)
+                port_til.error =
+                    String.format(resources.getString(R.string.err_at_least), portStr, 1)
                 return false
             }
             if (portAsInt > 65535) {
-                port_til.error = String.format(resources.getString(R.string.err_at_most), portStr, 65535)
+                port_til.error =
+                    String.format(resources.getString(R.string.err_at_most), portStr, 65535)
                 return false
             }
         } catch (ex: NumberFormatException) {
-            port_til.error = String.format(resources.getString(R.string.err_must_be_number), portStr)
+            port_til.error =
+                String.format(resources.getString(R.string.err_must_be_number), portStr)
             return false
         }
 
@@ -242,11 +249,18 @@ class ConnectDialog : AppCompatDialogFragment() {
         val ipAddress_til = binding.inputLayoutIpAddress
 
         if (ipAddressText == null || ipAddressText.isEmpty()) {
-            ipAddress_til.error = String.format(resources.getString(R.string.err_not_empty), ipaddressStr)
+            ipAddress_til.error =
+                String.format(resources.getString(R.string.err_not_empty), ipaddressStr)
             return false
         }
-
-        // TODO: more thorough validation
+        if (!Pattern.compile(
+                "((^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\$)|" +
+                        "^(^(?!\\bw{3}\\.\\b)[a-zA-Z0-9-_]{0,61}([a-zA-Z0-9])" +
+                        ".[a-zA-Z0-9-_]{1,})\\.[a-z]{2,4}\$)",
+                Pattern.CASE_INSENSITIVE).matcher(ipAddressText).find()) {
+            ipAddress_til.error = String.format(resources.getString(R.string.err_invalid), ipaddressStr)
+            return false
+        }
 
         ipAddress_til.error = null
         return true
@@ -256,11 +270,16 @@ class ConnectDialog : AppCompatDialogFragment() {
         val password_til = binding.inputLayoutPassword
 
         if (passwordText == null || passwordText.isEmpty()) {
-            password_til.error = String.format(resources.getString(R.string.err_not_empty), passwordStr)
+            password_til.error =
+                String.format(resources.getString(R.string.err_not_empty), passwordStr)
             return false
         }
 
-        // TODO: more thorough validation
+        val MINIMUM_PASSWORD_LENGTH = 6
+        if (passwordText.length < MINIMUM_PASSWORD_LENGTH) {
+            password_til.error = String.format(resources.getString(R.string.err_too_short), passwordStr)
+            return false
+        }
 
         password_til.error = null
         return true
